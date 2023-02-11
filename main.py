1f657c0d5137f8049c680cee4719d10814e2edf5
Sergey Morozov <se.mrzv@gmail.com>
2023-02-11 20:49:01 +0300

    wraps code into ifmain

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from utils import get_age, get_wines
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        winery_age=get_age(),
+        drinks=get_wines(),
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
 
-rendered_page = template.render(
-    winery_age=get_age(),
-    drinks=get_wines(),
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    main()
