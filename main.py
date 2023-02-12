0cd49cb0464cd9615828ae84ec19a1c469346f57
Sergey Morozov <se.mrzv@gmail.com>
2023-02-12 18:34:28 +0300

    adds argprase argument

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 from utils import get_age, get_wines
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Сайт-каталог алкогольной продукции')
+    parser.add_argument('file', help='Путь к файлу со списком товаров')
+    args = parser.parse_args()
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -14,7 +18,7 @@ def main():
 
     rendered_page = template.render(
         winery_age=get_age(),
-        drinks=get_wines(),
+        drinks=get_wines(args.file),
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
